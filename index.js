95371d8948df9573753d2083b6ef9c1ed179e4e5
Eddy2123 <141885842+Eddy2123@users.noreply.github.com>
2023-10-18 21:26:08 -0300

    ok

---
+++ index.js
@@ -0,0 +1,26 @@
+/*function numberimpar(nu1,) {
+const numero = nu1 % 2
+console.log(numero)
+}
+ numberimpar(4)
+let numero = numberimpar
+ if (numero === 0){
+    console.log(true)
+ }else {
+ console.log(false)
+}
+
+
+
+let parOuImpar1 = prompt ("inserir um numero")
+
+
+function parOuImpar(valor){
+    if(valor % 2 == 0){
+        console.log("É par");
+    } else {
+        console.log("É impar");
+    }
+}
+parOuImpar(parOuImpar1)
+*/
